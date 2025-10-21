17e1ee3559d1084e40312481fd5e9f93b6b335f0
ephraim <ephraims.818@gmail.com>
2025-10-21 06:48:09 +0600

    CODEWARS -- FUNDAMENTALS - 35. Abbreviate a Two Word Name

---
+++ Fundamentals/35. Abbreviate a Two Word Name/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function abbrevName(name) {
+    return name
+        .toUpperCase()
+        .split(" ")
+        .map((part) => part[0])
+
+        .join(".");
+}
+
+console.log(abbrevName("Sam Harris")); // "S.H"
+console.log(abbrevName("Patrick Feenan")); // "P.F"
+console.log(abbrevName("Evan Cole")); // "E.C"
+console.log(abbrevName("P Favuzzi")); // "P.F"
+console.log(abbrevName("David Mendieta")); // "D.M"
