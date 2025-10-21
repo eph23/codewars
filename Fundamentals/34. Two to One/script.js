26148c47478c828c241f4dfda549ff720bfb5754
ephraim <ephraims.818@gmail.com>
2025-10-21 06:21:41 +0600

    CODEWARS -- FUNDAMENTALS - 34. Two to One

---
+++ Fundamentals/34. Two to One/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function longest(s1, s2) {
+    let unique = new Set([...s1, ...s2]);
+    return [...unique].sort().join("");
+}
+
+console.log(longest("aretheyhere", "yestheyarehere")); // "aehrsty"
+console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding")); // "abcdefghilnoprstu"
+console.log(longest("inmanylanguages", "theresapairoffunctions")); // "acefghilmnoprstuy"
