56031111e2f201f6d5a4c2285eff3286cdc3e78b
ephraim <ephraims.818@gmail.com>
2025-10-23 07:21:20 +0600

    CODEWARS -- FUNDAMENTALS - 38. Returning Strings

---
+++ Fundamentals/38. Returning Strings/script.js
@@ -0,0 +1,7 @@
+"use strict";
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`;
+}
+
+console.log(greet("Ryan")); // "Hello, Ryan how are you doing today?";
+console.log(greet("Shingles")); // "Hello, Shingles how are you doing today?";
